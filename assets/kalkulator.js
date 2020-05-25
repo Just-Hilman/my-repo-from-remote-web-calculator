e06c63c2b066b53d62e1d09e54eaf9e2caf654c5
Just-Hilman <hilman620@yahoo.com>
2020-05-25 12:14:01 +0700

    added function into content

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -4,3 +4,18 @@ const calculator {
     firtsNumber: null;
     waitingForSecondNumber: false;
 }
+
+function updateDisplay() {
+    document.querySelector("#displayNumber").innerText = calculator.displayNumber;
+}
+
+function clearCalculator() {
+    calculator.displayNumber = '0';
+    calculator.operator = null;
+    calculator.firtsNumber = null;
+    calculator.waitingForSecondNumber = false;
+}
+
+function inputDigit(digit) {
+    calculator.displayNumber += digit;
+}
