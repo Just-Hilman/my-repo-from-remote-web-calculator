220c8a3dd65b02bd5dbe1a67e5159cce7a83fd89
Just-Hilman <hilman620@yahoo.com>
2020-05-25 14:09:06 +0700

    added function into content

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -1,7 +1,7 @@
 const calculator = {
     displayNumber: '0',
     operator: null,
-    firtsNumber: null,
+    firstNumber: null,
     waitingForSecondNumber: false
 };
 
@@ -12,16 +12,55 @@ function updateDisplay() {
 function clearCalculator() {
     calculator.displayNumber = '0';
     calculator.operator = null;
-    calculator.firtsNumber = null;
+    calculator.firstNumber = null;
     calculator.waitingForSecondNumber = false;
 }
 
 function inputDigit(digit) {
-    if (calculator.displayNumber === '0') {
+    if (calculator.waitingForSecondNumber && calculator.firstNumber === calculator.displayNumber) {
         calculator.displayNumber = digit;
     } else {
-        calculator.displayNumber += digit;
+        if (calculator.displayNumber === '0') {
+            calculator.displayNumber = digit;
+        } else {
+            calculator.displayNumber += digit;
+        }
     }
+}
+
+calculator.fi
+
+function inverseNumber() {
+    if (calculator.displayNumber === '0') {
+        return;
+    }
+    calculator.displayNumber = calculator.displayNumber * -1;
+}
+
+function handleOperator(operator) {
+    if (!calculator.waitingForSecondNumber) {
+        calculator.operator = operator;
+        calculator.waitingForSecondNumber = true;
+        calculator.firstNumber = calculator.displayNumber;
+    } else {
+        alert('Operator sudah ditetapkan')
+    }
+}
+
+function performCalculation() {
+    if (calculator.firstNumber == null || calculator.operator == null) {
+        alert("Anda belum menetapkan operator");
+        return;
+    }
+
+    let result = 0;
+    if (calculator.operator === "+") {
+        result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+    } else {
+        result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber)
+    }
+
+    calculator.displayNumber = result;
 }
 
 const buttons = document.querySelectorAll(".button");
@@ -30,6 +69,30 @@ for (let button of buttons) {
 
         // mendapatkan objek elemen yang diklik
         const target = event.target;
+
+        if (target.classList.contains('clear')) {
+            clearCalculator();
+            updateDisplay();
+            return;
+        }
+
+        if (target.classList.contains('negative')) {
+            inverseNumber();
+            updateDisplay();
+            return;
+        }
+
+        if (target.classList.contains('equals')) {
+            performCalculation();
+            updateDisplay();
+            return;
+        }
+
+        if (target.classList.contains('operator')) {
+            handleOperator(target.innerText);
+            updateDisplay();
+            return;
+        }
 
         inputDigit(target.innerText);
         updateDisplay()
